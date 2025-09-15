8ed50161a71319d63d501f734a3f72473efe4fa1
jonathanp2004 <jpadilla1782.2@gmail.com>
2025-09-15 00:20:30 -0500

    Populated spinner and did some touches up on UI

--- app/src/main/java/com/example/poke_trackerapp/MainActivity.java
+++ app/src/main/java/com/example/poke_trackerapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.poke_trackerapp;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +10,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner levelSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        levelSpinner = findViewById(R.id.levelSpinner);
+        ArrayList<String> lvls = new ArrayList<>();
+        for(int i =1; i<=50; i++) lvls.add(String.valueOf(i));
+        ArrayAdapter<String> levelAdapter = new ArrayAdapter<>(
+                    this, android.R.layout.simple_spinner_item, lvls);
+        levelAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        levelSpinner.setAdapter(levelAdapter);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.attackTE), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
