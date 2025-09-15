d6471a01b0559df415e22839397522c34673a0e9
jonathanp2004 <jpadilla1782.2@gmail.com>
2025-09-15 00:59:49 -0500

    Add save button and reset functionality; update input types for number fields

--- app/src/main/java/com/example/poke_trackerapp/MainActivity.java
+++ app/src/main/java/com/example/poke_trackerapp/MainActivity.java
@@ -19,9 +19,10 @@ public class MainActivity extends AppCompatActivity {
 
     private Spinner levelSpinner;
 
-    private EditText numberEt, nameET, speciesEt, heightET,weightET, hpET, attackET, defenseET;
+    private EditText numberET, nameET, speciesEt, heightET,weightET, hpET, attackET, defenseET;
     private RadioButton maleButton, unkButton, femaleButton;
     private Button resetButton;
+    private Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +32,13 @@ public class MainActivity extends AppCompatActivity {
 
         levelSpinner = findViewById(R.id.levelSpinner);
 
-        numberEt = findViewById(R.id.numberET);
+        numberET = findViewById(R.id.numberET);
         nameET = findViewById(R.id.nameET);
         speciesEt = findViewById(R.id.speicesET);
         heightET = findViewById(R.id.heightET);
         weightET = findViewById(R.id.weightET);
         hpET = findViewById(R.id.hpET);
-        attackET = findViewById(R.id.attackTE);
+        attackET = findViewById(R.id.attackET);
         defenseET = findViewById(R.id.defenseET);
 
         femaleButton = findViewById(R.id.femaleButton);
@@ -45,6 +46,11 @@ public class MainActivity extends AppCompatActivity {
         unkButton = findViewById(R.id.unkButton);
 
         resetButton = findViewById(R.id.resetButton);
+        saveButton = findViewById(R.id.saveButton);
+
+
+
+
 
 
 
@@ -55,11 +61,42 @@ public class MainActivity extends AppCompatActivity {
         levelAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         levelSpinner.setAdapter(levelAdapter);
 
+        reset();
+        resetButton.setOnClickListener(v -> reset());
+
+
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.attackTE), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
+
+    }
+    private void reset()
+    {
+        numberET.setText("777");
+        nameET.setText("Pikachu");
+        speciesEt.setText("Mouse Pokemon");
+        heightET.setText("0.4 m");
+        weightET.setText("6.0 kg");
+        hpET.setText("35");
+        attackET.setText("55");
+        defenseET.setText("40");
+
+        if(levelSpinner.getAdapter() != null && levelSpinner.getAdapter().getCount()>0)
+        {
+            levelSpinner.setSelection(0);
+        }
+        femaleButton.setChecked(false);
+        maleButton.setChecked(false);
+        unkButton.setChecked(false);
+    }
+
+    private void save()
+    {
+
     }
 }
