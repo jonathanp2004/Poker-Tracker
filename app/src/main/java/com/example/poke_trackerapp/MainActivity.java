359ff0ff8e2d34b7ff44ae7516f312a64201c18d
jonathanp2004 <jpadilla1782.2@gmail.com>
2025-09-16 16:11:35 -0500

    Update layout reference in MainActivity to use linear.xml instead of table.xml

--- app/src/main/java/com/example/poke_trackerapp/MainActivity.java
+++ app/src/main/java/com/example/poke_trackerapp/MainActivity.java
@@ -34,8 +34,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        //setContentView(R.layout.linear);
-        setContentView(R.layout.table);
+        setContentView(R.layout.linear);
+        //setContentView(R.layout.table);
         //setContentView(R.layout.constraint);
 
         levelSpinner = findViewById(R.id.levelSpinner);
