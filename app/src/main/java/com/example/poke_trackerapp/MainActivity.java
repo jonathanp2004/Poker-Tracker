1b5af7b5e748a4ba9c7f8520fb0736700d8ee36c
jonathanp2004 <jpadilla1782.2@gmail.com>
2025-09-16 16:07:50 -0500

    Refactor layout structure: replace ConstraintLayout with TableLayout in table.xml; update activity_main.xml to use new layout; enhance input fields and add gender selection

--- app/src/main/java/com/example/poke_trackerapp/MainActivity.java
+++ app/src/main/java/com/example/poke_trackerapp/MainActivity.java
@@ -34,7 +34,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.linear);
+        setContentView(R.layout.table);
+        //setContentView(R.layout.constraint);
 
         levelSpinner = findViewById(R.id.levelSpinner);
 
